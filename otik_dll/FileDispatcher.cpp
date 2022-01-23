151a6df117a9e75621c21f80bc499966154c2442
just-random-user <sineokovivan@mail.ru>
2022-01-23 18:46:27 +0300

    Fixed compression 1.0

--- otik_dll/FileDispatcher.cpp
+++ otik_dll/FileDispatcher.cpp
@@ -10,13 +10,14 @@ string file_create_metadata(string filepath_s, uint64_t filesize)
 {
 	uint16_t metadata_size = 16 + 64 + filepath_s.size();
 	string res = string();
-	
+
 	res.append(16 - std::to_string(metadata_size).length(), '0');
 	res.append(std::to_string(metadata_size));
 
 	res.append(64 - std::to_string(filesize).length(), '0');
 	res.append(std::to_string(filesize));
 
+	std::replace(filepath_s.begin(), filepath_s.end(), '\\', '/');
 	res.append(filepath_s);
 
 	return res;
@@ -24,9 +25,11 @@ string file_create_metadata(string filepath_s, uint64_t filesize)
 
 string get_file_content(string path_to_file)
 {
-	std::ifstream file = std::ifstream(path_to_file);
+	std::ifstream file = std::ifstream(path_to_file, std::ios_base::binary);
+
 	string content((std::istreambuf_iterator<char>(file)),
 		(std::istreambuf_iterator<char>()));
+	//string content = string();
 	return content;
 }
 
--- otik_dll/StringDispatcher.h
+++ otik_dll/StringDispatcher.h
@@ -4,4 +4,4 @@
 #include <string>
 #include "pch.h"
 
-std::vector<std::string> dispatch_filenames(char* files_names, int length);
+std::vector<std::string> dispatch_filenames(const char* files_names, int length);
